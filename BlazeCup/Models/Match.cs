1327c9289b531ba2ffed241fe0d9681aed9b6d2a
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2018-06-18 23:37:59 +0200

    Colour coded match results.

--- BlazeCup/Models/Match.cs
+++ BlazeCup/Models/Match.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlazeCup.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,5 +14,18 @@ namespace BlazeCup.Models
         public int AwayPredictedGoals { get; set; }
         public int HomeActualGoals { get; set; }
         public int AwayActualGoals { get; set; }
+
+        public int Points
+        {
+            get
+            {
+                if (HomeActualGoals == -1 || AwayActualGoals == -1)
+                {
+                    return -1;
+                }
+
+                return Scoring.ScoreGame(HomePredictedGoals, AwayPredictedGoals, HomeActualGoals, AwayActualGoals);
+            }
+        }
     }
 }
