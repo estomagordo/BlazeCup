7face7a362cce7978fe0c80c2b9402a4630a0880
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2018-06-25 20:43:44 +0200

    Showing colours for through group, up until champions.

---
+++ BlazeCup/Enums/Enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazeCup.Enums
+{
+    public class Enums
+    {
+        public enum TeamStage
+        {
+            RunnersUp,
+            OutSemi,
+            OutQuarter,
+            Out16,
+            Outgroup,
+            Undecided,
+            ThroughGroup,
+            Through16,
+            ThroughQuarter,
+            ThroughSemi,
+            Champions
+        }
+    }
+}
--- BlazeCup/Models/Team.cs
+++ BlazeCup/Models/Team.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static BlazeCup.Enums.Enums;
 
 namespace BlazeCup.Models
 {
@@ -9,5 +10,66 @@ namespace BlazeCup.Models
     {
         public string Name { get; set; }
         public string FlagUrl { get; set; }
+        public TeamStage Stage { get; set; }
+
+        public string SecondRoundColour
+        {
+            get
+            {
+                return Stage == TeamStage.Outgroup ?
+                    "colour-red" :
+                    Stage == TeamStage.Undecided ?
+                        string.Empty :
+                        "colour-green";
+            }
+        }
+
+        public string QuarterFinalColour
+        {
+            get
+            {
+                return (Stage <= TeamStage.Outgroup && Stage >= TeamStage.Out16) ?
+                    "colour-red" :
+                    (Stage <= TeamStage.ThroughGroup && Stage >= TeamStage.Undecided) ?
+                        string.Empty :
+                        "colour-green";
+            }
+        }
+
+        public string SemiFinalColour
+        {
+            get
+            {
+                return (Stage <= TeamStage.Outgroup && Stage >= TeamStage.OutQuarter) ?
+                    "colour-red" :
+                    (Stage <= TeamStage.Through16 && Stage >= TeamStage.Undecided) ?
+                        string.Empty :
+                        "colour-green";
+            }
+        }
+
+        public string FinalColour
+        {
+            get
+            {
+                return (Stage <= TeamStage.Outgroup && Stage >= TeamStage.OutSemi) ?
+                    "colour-red" :
+                    (Stage <= TeamStage.ThroughQuarter && Stage >= TeamStage.Undecided) ?
+                        string.Empty :
+                        "colour-green";
+            }
+        }
+
+        public string ChampionColour
+        {
+            get
+            {
+                return (Stage <= TeamStage.Outgroup && Stage >= TeamStage.RunnersUp) ?
+                    "colour-red" :
+                    (Stage <= TeamStage.ThroughSemi && Stage >= TeamStage.Undecided) ?
+                        string.Empty :
+                        "colour-green";
+            }
+        }
     }
 }
